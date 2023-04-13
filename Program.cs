6cb0b27bf72eaa0f05c878d5dca1a7a156730858
AnnaJanu <anna.janusko@gmail.com>
2023-04-13 19:33:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPShapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle1 = new Rectangle(3, 8);
+
+            Console.WriteLine($"Area of the rectangle: {rectangle1.GetArea()}");
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPShapes
+{
+    internal class Rectangle : Shape
+    {
+        private int sideA;
+        private int sideB;
+
+        public Rectangle(int A, int B)
+        {
+            this.sideA = A;
+            this.sideB = B;
+        }
+        public override int GetArea()
+        {
+            return this.sideA * this.sideB;
+        }
+    }
+}
